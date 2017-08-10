1647adf31f4384ce4b28884c5cfbc620b87d7b45
= <twshere@outlook.com>
2017-08-10 14:03:01 +0000

    add-arrow-transform

--- test/test_arrow.py
+++ test/test_arrow.py
@@ -9,3 +9,16 @@ assert range(100) -> reduce(.x,y->x+y, _) -> print(_)  or True where:
 print('pass: ...')
 
 
+foreach = .self, f -> None where:
+        for item in self:
+            f(item)
+
+groupby = .self, f -> ret where:
+        ret = defaultdict(list) where:
+                from collections import defaultdict
+        self -> foreach(_, setv) where:
+                setv = .item -> None where:
+                    ret[f(item)].append(item)
+        
+range(20) -> groupby(_, .x->x%2) -> print(_)
+
