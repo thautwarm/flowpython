11c4ecba267f82d4b1118f57fc536e6b0d43b00e
= <twshere@outlook.com>
2017-08-12 21:44:36 +0000

    conf

---
+++ flowpy_src/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import io
+with io.open('./flowpython/ReadMe.rst', encoding='utf-8') as f:
+    readme = f.read()
+import os
+cat = os.path.join
+setup(
+    name = 'flowpython',
+    version = '0.1',
+    keywords='gramamr, ast, readability',
+    description = "Grammar extensions for Python",
+    long_description=readme,
+    license = 'MIT License',
+    url = 'https://github.com/thautwarm/flowpython',
+    author = 'Thautwarm',
+    author_email = 'twshere@outlook.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms  = ['windows','linux'],
+    classifiers=['Programming Language :: Python :: 3.6','Programming Language :: Python :: Implementation :: CPython']
+)
+cat(os.environ["HOME"], '.flowpy') -> os.remove(_) if os.path.exists(_) else None
--- flowpython/utils.py
+++ flowpython/utils.py
@@ -8,14 +8,14 @@ def makedir_from(file):
 
 def bin_copyto(file_from, file_to):
 	if not os.path.exists(file_from):
-		raise f"{file_from} not exists!"
+		raise BaseException(f"{file_from} not exists!")
 	print(f"writing {file_from} to {file_to}...")
 	with open(file_to, 'wb') as _to, open(file_from,'rb') as _from:
 		_to.write(_from.read())
 
 def moveto(file_from, file_to):
 	if not os.path.exists(file_from):
-		raise f"{file_from} not exists!"
+		raise BaseException(f"{file_from} not exists!")
 	print(f"moving {file_from} to {file_to}...")
 	os.rename(file_from, file_to)
 
