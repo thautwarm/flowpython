e7f08c087faca1bfba4b071b999c3640c2e23be4
thautwarm <twshere@outlook.com>
2017-08-25 23:10:07 +0800

    add-pipeline

--- test/f_test.py
+++ test/f_test.py
@@ -9,6 +9,8 @@ f = .x -> r where:
 
         otherwise           =>  r= a + f(b)
 
+        
+
 from flowpython.fp import flow_map, flat_map,flatten, andThen, fastmap, flow_reduce
 a = [[1,2,3],[3,4,6], [2,3,4,5,6,7,8],[2,3,4,5,1,2,3]]
 fast_flat_map = lambda f: andThen(flatten)(fastmap(f))
