ca7f15d3cab1933543a7165270faa83b76e65c23
thautwarm <twshere@outlook.com>
2017-08-28 20:04:39 +0800

    begin-support-py35

--- test/forfun.py
+++ test/forfun.py
@@ -12,12 +12,9 @@ this module is just for fun!!!
 from copy import deepcopy
 permutations = .seq -> seq_seq where:
     condic+[] seq:        
-        case (a,  ) => seq_seq = [a,]        
-        case (a, b) => seq_seq = [[a,b],[b,a]]        
+        case (a,  ) => seq_seq = [[a,]]              
         case (a,*b) => 
             seq_seq = permutations(b) -> map(.x -> insertAll(x, a),  _) -> sum(_, []) where:
                 insertAll = . x, a -> ret where:
                     ret = [ deepcopy(x) -> _.insert(i, a) or _ for i in  (len(x) -> range(_+1))  ]
-                        
-            
 a = range(3) -> permutations(_)
